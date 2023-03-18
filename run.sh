db0d62be17bd2958b689fb804904a9dae32c4413
HYUN WOOK KANG <kangtaepoong@naver.com>
2023-03-18 14:57:14 +0900

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,7 @@
+python facialExpressionRecognitionResNet.py --data-folder /media/jeff/external/data/RAF-DB --center
+
+python facialExpressionRecognitionResNet.py --data-folder /media/jeff/external/data/RAF-DB
+
+python facialExpressionRecognitionResNet.py --data-folder /home/jeff/data/FER2013 --center
+
+python facialExpressionRecognitionResNet.py --data-folder /home/jeff/data/FER2013
